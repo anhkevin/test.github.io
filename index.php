193f417025be11d294e8efdfc327338a0e773d95
Ánh Kevin <anh_kevin@hotmail.com>
2021-08-03 23:21:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Redirect
-    <script>
-        let urlCurrent = window.location.href;
-        let urlNew = urlCurrent.replace("anhkevin.github.io", "tiandev.net");
-        window.location.href(urlNew);
-    </script>
-</body>
-</html>
+<?php
+header('HTTP/1.1 301 Moved Permanently');
+header('Location: https://tiandev.net/');
+exit();
